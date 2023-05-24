5a7dd722b09fc35e6c2b3b94ee8d6e215c71cd91
haojinhui <3526250846@qq.com>
2023-05-24 13:33:14 +0800

    解决room的jdk冲突问题

--- app/src/main/java/com/example/note/MainActivity.kt
+++ app/src/main/java/com/example/note/MainActivity.kt
@@ -3,7 +3,19 @@ package com.example.note
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.room.Dao
+import androidx.room.Database
+import androidx.room.Delete
+import androidx.room.Entity
+import androidx.room.Insert
+import androidx.room.PrimaryKey
+import androidx.room.Query
+import androidx.room.Room
+import androidx.room.RoomDatabase
 import com.example.note.databinding.ActivityMainBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     private val notes: MutableList<Note> = mutableListOf()
@@ -25,6 +37,19 @@ class MainActivity : AppCompatActivity() {
             }
         }
         update()
+
+        val db = Room.databaseBuilder(
+            applicationContext,
+            AppDatabase::class.java, "database-name"
+        ).build()
+
+        val userDao = db.userDao()
+
+        GlobalScope.launch(Dispatchers.IO) {
+            val users = userDao.getAll()
+
+        }
+
     }
 
     private fun add(note: Note) {
@@ -36,3 +61,28 @@ class MainActivity : AppCompatActivity() {
         adapter.notifyDataSetChanged()
     }
 }
+
+@Entity(tableName = "user")
+data class User(
+    @PrimaryKey val id: Int,
+    val name: String,
+    val age: Int
+)
+
+@Dao
+interface UserDao {
+    @Query("SELECT * FROM user")
+    fun getAll(): List<User>
+
+    @Insert
+    fun insertAll(vararg users: User)
+
+    @Delete
+    fun delete(user: User)
+}
+
+@Database(entities = arrayOf(User::class), version = 1)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun userDao(): UserDao
+}
+
